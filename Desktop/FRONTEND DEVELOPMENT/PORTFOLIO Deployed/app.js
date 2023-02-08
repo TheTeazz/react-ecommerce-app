3bd12f0dfb3fa1b4a39411f447fb50df4659b2ce
Teazz <etosin45@gmail.com>
2023-02-08 20:45:30 +0100

    minor fixes

---
+++ Desktop/FRONTEND DEVELOPMENT/PORTFOLIO Deployed/app.js
@@ -0,0 +1,7 @@
+let controls = document.querySelectorAll('.control')
+let section =document.querySelector('.section')
+let sections = Array.from(document.querySelectorAll('.section'))
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+
+
